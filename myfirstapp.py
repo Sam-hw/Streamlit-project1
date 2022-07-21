63c62a9f71464f26783c77405b95422bef8872d4
Sam-hw <109711096+Sam-hw@users.noreply.github.com>
2022-07-21 12:04:00 +0800

    Create myfirstapp.py

---
+++ myfirstapp.py
@@ -0,0 +1,7 @@
+echo "# Streamlit-project1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M 'main'
+git remote add origin https://github.com/Sam-hw/Streamlit-project1.git
+git push -u origin 'main'
